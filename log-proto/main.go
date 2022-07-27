7bd88f2d23e7009b2056c26bc684896da58c3313
kamidzuua <kamidzu.new@gmail.com>
2022-07-27 23:46:51 +0300

    added automatic logdir getter

--- log-proto/main.go
+++ log-proto/main.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
+	"os/user"
+	"strings"
 
 	"github.com/hpcloud/tail"
 )
 
-var powloc = "/home/kamidzu/.wine.hearthstone/dosdevices/c:/Program Files/Hearthstone/Logs/Power.log"
+func FindLogDir() string {
+	var logdir string
+	var prefix string
 
+	user, err := user.Current()
+	prefix = "/home/" + user.Username + "/"
+	cmd := exec.Command("bash", "-c", "find | grep Power.log")
+	cmd.Dir = prefix
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	split := strings.Split(strings.TrimSpace((string(output))), "/")
+
+	logdir = strings.Join(split[1:], "/")
+	logdir = prefix + logdir
+	return logdir
+}
+
+//TODO Really Parse Lines
 func ParseLine(line string) {
 	fmt.Println(line)
 }
 
 func main() {
+
+	powloc := FindLogDir()
+
 	t, err := tail.TailFile(powloc, tail.Config{Follow: true})
 
 	if err != nil {
